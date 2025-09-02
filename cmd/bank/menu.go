245bb59cd4fb04937f252084be4c2fcec17a281b
Vinicius Lima <viniciusalmeidalima@icloud.com>
2025-09-02 14:20:55 -0300

    Utilizando package's de terceiros

--- cmd/bank/menu.go
+++ cmd/bank/menu.go
@@ -2,10 +2,13 @@ package main
 
 import (
 	"fmt"
+
+	randomdata "github.com/Pallinder/go-randomdata"
 )
 
 func exibirMenu() {
 	fmt.Println("Bem vindo ao Banco Go!")
+	fmt.Println("Cliente:", randomdata.FullName(randomdata.RandomGender), "\n")
 	fmt.Println("O que deseja fazer?!")
 	fmt.Println("1. Verificar Saldo")
 	fmt.Println("2. Depositar")
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module studying_golang
 
 go 1.24.6
+
+require github.com/Pallinder/go-randomdata v1.2.0 // indirect
