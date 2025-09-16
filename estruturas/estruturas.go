12b90d01793d94993e2d84bb5edf46c95295091c
Vinicius Lima <viniciusalmeidalima@icloud.com>
2025-09-16 15:21:28 -0300

    Criação de estrutura usando outra estrutura | mudanças de teste no admin

--- estruturas/estruturas.go
+++ estruturas/estruturas.go
@@ -9,17 +9,25 @@ func main() {
 	userNome := dadosUsuario("Digite seu nome: ")
 	userSobrenome := dadosUsuario("Digite seu sobrenome: ")
 	userAniversario := dadosUsuario("Digite sua data de aniversário (dd/mm/aaaa): ")
+	userAdmin := dadosUsuario("Você é administrador? (s/n): ")
 
-	usuario, err := usuario.NovoUsuario(userNome, userSobrenome, userAniversario)
+	if userAdmin == "s" || userAdmin == "S" {
+		admin := usuario.NewAdministrador("admin@exemplo.com", "senha123", userNome, userSobrenome, userAniversario)
+		admin.Usuario.PrintInfosAdmin()
 
-	if err != nil {
-		fmt.Println("Erro ao criar usuário:", err)
-		return
+	} else {
+		user, err := usuario.New(userNome, userSobrenome, userAniversario)
+
+		if err != nil {
+			fmt.Println("Erro ao criar usuário:", err)
+			return
+		}
+
+		user.PrintInfos()
+		user.LimparNome()
+		user.PrintInfos()
 	}
 
-	usuario.PrintInfos()
-	usuario.LimparNome()
-	usuario.PrintInfos()
 }
 
 func dadosUsuario(textoPergunta string) string {
--- estruturas/usuario/usuario.go
+++ estruturas/usuario/usuario.go
@@ -13,8 +13,20 @@ type Usuario struct {
 	criadoEm    time.Time
 }
 
+type Administrador struct {
+	email string
+	senha string
+	Usuario
+}
+
 func (u Usuario) PrintInfos() { //Método associado a struct Usuario
 	fmt.Println("Nome: ", u.Nome, u.Sobrenome, "Aniversário: ", u.Aniversario, "Criado em: ", u.criadoEm.Format("02/01/2006"))
+}
+
+func (u Usuario) PrintInfosAdmin() { //Método associado a struct Usuario
+	fmt.Println("Nome admin: ", u.Nome, u.Sobrenome)
+	fmt.Println("Aniversário: ", u.Aniversario)
+	fmt.Println("Criado em: ", u.criadoEm.Format("02/01/2006"))
 }
 
 func (u *Usuario) LimparNome() {
@@ -23,10 +35,23 @@ func (u *Usuario) LimparNome() {
 
 	// Utilizamos o * para alterar o valor do ponteiro
 	// Se não utilizássemos o * estaríamos alterando apenas uma cópia do valor
-	// Ou seja, a alteração não seria refletida fora do método
+	// Ou seja, a alteração não se refletiria fora do método
 }
 
-func NovoUsuario(nome, sobrenome, aniversario string) (*Usuario, error) {
+func NewAdministrador(email, senha, nome, sobrenome, aniversario string) Administrador {
+	return Administrador{
+		email: email,
+		senha: senha,
+		Usuario: Usuario{
+			Nome:        nome,
+			Sobrenome:   sobrenome,
+			Aniversario: aniversario,
+			criadoEm:    time.Now(),
+		},
+	}
+}
+
+func New(nome, sobrenome, aniversario string) (*Usuario, error) {
 	if nome == "" || sobrenome == "" || aniversario == "" {
 		return nil, errors.New("Nome/Sobrenome/Aniversário são campos obrigatórios")
 	}
