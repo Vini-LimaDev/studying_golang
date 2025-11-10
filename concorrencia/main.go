dc2769786303bfb4db5ecf2642df23b360fa9c4a
Vinicius Lima <viniciusalmeidalima@icloud.com>
2025-11-10 14:25:14 -0300

    Usando channels

--- concorrencia/main.go
+++ concorrencia/main.go
@@ -6,18 +6,21 @@ import (
 )
 
 func main() {
-	go chamadaSemSleep("Muito prazer em te conhecer!")
-	go chamadaSemSleep("Estou aprendendo Go!")
-	go chamadaComSleep("Concorrência é igual um await no JS!")
-	// A chamada abaixo só será exibida após 4 segundos, pois a função acima possui um sleep de 4 segundos
-	go chamadaComSleep("Finalizando as chamadas!")
+	// go chamadaSemSleep("Muito prazer em te conhecer!")
+	// go chamadaSemSleep("Estou aprendendo Go!")
+	done := make(chan bool)
+	go chamadaComSleep("Concorrência é igual um await no JS!", done)
+	// // A chamada abaixo só será exibida após 4 segundos, pois a função acima possui um sleep de 4 segundos
+	// go chamadaComSleep("Finalizando as chamadas!")]
+	<-done
 }
 
 func chamadaSemSleep(texto string) {
 	fmt.Println(texto)
 }
 
-func chamadaComSleep(texto string) {
+func chamadaComSleep(texto string, done chan bool) {
 	time.Sleep(4 * time.Second)
 	fmt.Println(texto)
+	done <- true
 }
